054b98ef1741a37a1f474e3b72dc3dfe7442a6dc
prashant <prashant.p@geitpl.com>
2018-12-28 09:44:20 -0800

    second commit

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home' , views.homepage),
+    path('' , views.homepage),
     path('counts/' ,views.count , name='count')
     
 ]
